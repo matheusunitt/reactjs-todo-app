07af4b66260b4b749d4dcd73d8873da09d3f6c93
Jeffrey Matheus <matheusunitt@gmail.com>
2020-07-11 14:06:03 -0300

    Initial project setup; Styled components iinstalled;

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: var(--black);
+  }
+  html, body, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+    width: 100%;
+    height: 100%;
+  }
+  *, input, button {
+    border: 0;
+    font-family: 'Segoe UI', Roboto, 'Helvetica Neue', Ubuntu, Arial, sans-serif;
+  }
+  html {
+    background-color: var(--white);
+  }
+  :root {
+    --white: #fff;
+    --black: #000;
+    --primary: blue;
+    --secondary: red;
+    --gray: #ccc;
+  }
+`;
